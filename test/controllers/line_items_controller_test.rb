2330eadcd24328077869cd10b3e85a9016e65e90
Szymon Soczewka <sz.soczewka@gmail.com>
2020-07-04 10:52:58 +0200

    A Smarter Cart

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -17,12 +17,14 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create line_item" do
     assert_difference('LineItem.count') do
-      post line_items_url, params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+      post line_items_url, params: { product_id: products(:ruby).id }
     end
-
-    assert_redirected_to line_item_url(LineItem.last)
-  end
-
+    follow_redirect!
+      assert_select 'h2', 'Your Cart'
+      assert_select 'td', "Programming Ruby 1.9"
+    end
+  
+  
   test "should show line_item" do
     get line_item_url(@line_item)
     assert_response :success
@@ -34,7 +36,8 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update line_item" do
-    patch line_item_url(@line_item), params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+    patch line_item_url(@line_item),
+    params: { line_item: { product_id: @line_item.product_id } }
     assert_redirected_to line_item_url(@line_item)
   end
 
